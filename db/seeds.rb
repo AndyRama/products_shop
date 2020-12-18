8715e821f966a9774dd21ca243d65c5316cf194a
AndyRama <andyramaroson@gmail.com>
2020-12-18 23:22:44 +0100

    controller products + models product/category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database...' 
+Product.destroy_all if Rails.env.development?
+Category.destroy_all if Rails.env.development?
+
+puts'Creating categories...'
+nano = Category.create!(name: 'nano')
+console = Category.create!(name: 'console')
+
+puts ' Creating products...'
+Product.create!(sku: 'N64 PI MINI', name:'PI N64', category: nano, photo_url: '64PI4.PNG')
+Product.create!(sku: 'XU4 64', name:'XU4 N64', category: nano, photo_url: 'CONSOLEX.PNG')
+Product.create!(sku: 'Console X', name:'X N64', category: console, photo_url: 'XU4N64.PNG')
+puts 'Finished!'
