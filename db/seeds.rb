6996e5c4388e2bfa41c49c07941bd468cbd48081
AndyRama <andyramaroson@gmail.com>
2020-12-18 23:54:28 +0100

    login custom

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,7 @@ puts ' Creating products...'
 Product.create!(sku: 'N64 PI MINI', name:'PI N64', category: nano, photo_url: '64PI4.PNG')
 Product.create!(sku: 'XU4 64', name:'XU4 N64', category: nano, photo_url: 'CONSOLEX.PNG')
 Product.create!(sku: 'Console X', name:'X N64', category: console, photo_url: 'XU4N64.PNG')
+Product.create!(sku: 'N64 MINI', name:'N64', category: nano, photo_url: '64PI4.PNG')
+Product.create!(sku: 'XU4 64', name:'XU4', category: nano, photo_url: 'CONSOLEX.PNG')
+Product.create!(sku: 'Console X', name:'X N', category: console, photo_url: 'XU4N64.PNG')
 puts 'Finished!'
