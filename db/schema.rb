0165a7956e8d8643793426745972165add92397f
AndyRama <andyramaroson@gmail.com>
2020-12-19 02:27:00 +0100

    db schema/seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_175603) do
+ActiveRecord::Schema.define(version: 2020_12_19_000638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_12_18_175603) do
     t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price_cents", default: 0, null: false
+    t.string "price_currency", default: "EUR", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ nano = Category.create!(name: 'nano')
 console = Category.create!(name: 'console')
 
 puts ' Creating products...'
-Product.create!(sku: 'N64 PI MINI', name:'PI N64', category: nano, photo_url: '64PI4.PNG')
-Product.create!(sku: 'XU4 64', name:'XU4 N64', category: nano, photo_url: 'CONSOLEX.PNG')
-Product.create!(sku: 'Console X', name:'X N64', category: console, photo_url: 'XU4N64.PNG')
-Product.create!(sku: 'N64 MINI', name:'N64', category: nano, photo_url: '64PI4.PNG')
-Product.create!(sku: 'XU4 64', name:'XU4', category: nano, photo_url: 'CONSOLEX.PNG')
-Product.create!(sku: 'Console X', name:'X N', category: console, photo_url: 'XU4N64.PNG')
+Product.create!(sku: 'N64 PI MINI', name:'PI N64', price_cents:'2000',category: nano, photo_url: '64PI4.PNG')
+Product.create!(sku: 'XU4 64', name:'XU4 N64', price_cents:'5000', category: nano, photo_url: 'CONSOLEX.PNG')
+Product.create!(sku: 'Console X', name:'X N64', price_cents:'4000', category: console, photo_url: 'XU4N64.PNG')
+Product.create!(sku: 'N64 MINI', name:'N64',price_cents:'6000', category: nano, photo_url: '64PI4.PNG')
+Product.create!(sku: 'XU4 64', name:'XU4', price_cents:'10000', category: nano, photo_url: 'CONSOLEX.PNG')
+Product.create!(sku: 'Console X', name:'X N', price_cents:'3000', category: console, photo_url: 'XU4N64.PNG')
 puts 'Finished!'
